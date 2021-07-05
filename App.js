1701d624e504d1f1b3e4315cdbd9b9277c934b10
CrenierAmaury <55687534+CrenierAmaury@users.noreply.github.com>
2021-07-06 01:10:55 +0200

    git rm cached pour fichiers de config et debut de gestion de contexte

--- App.js
+++ App.js
@@ -1,35 +1,61 @@
 import React from 'react';
 import firebase from 'firebase';
-import {StyleSheet} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import {firebaseConfig} from './firebaseConfig';
+import {StyleSheet, Text, View} from 'react-native';
+//import {createStackNavigator} from '@react-navigation/stack';
+//import {NavigationContainer} from '@react-navigation/native';
 
 //initialization of firebase
-let firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
-};
-
 firebase.initializeApp(firebaseConfig);
 
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoading: true,
+      isSignedIn: false,
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this._checkIfLoggedIn();
+  }
+
+  _checkIfLoggedIn() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('1');
+        this.isLoading = false;
+        this.state.isSignedIn = true;
+        this.state.user.email = user.email;
+      } else {
+        console.log('2');
+        this.isLoading = false;
+        this.state.isSignedIn = false;
+      }
+    });
   }
 
   render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator></Stack.Navigator>
-      </NavigationContainer>
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <Text>LOADING</Text>
+        </View>
+      );
+    }
+
+    return this.state.isSignedIn ? (
+      <View>
+        <Text>{this.state.user.email}</Text>
+      </View>
+    ) : (
+      <View>
+        <Text>NON CONNECTE</Text>
+      </View>
     );
   }
 }
