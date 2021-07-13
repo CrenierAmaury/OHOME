b4962b400deb48e05cce52b5e529e29ff682b1f1
CrenierAmaury <amaurycrenier@gmail.com>
2021-07-13 22:26:20 +0200

    suite overlay ajout dépense, gestion balance
    overlay dans le fichier budget screen
    ajout d'un picker de catégorie
    gestion des placeholders et labels
    calcul et synchronisation de la balance
    ajout des api remove et update

--- app/api/budgetApi.js
+++ app/api/budgetApi.js
@@ -1,6 +1,21 @@
 import firestore from '@react-native-firebase/firestore';
 
-export async function getOverview(budgetId) {
+async function updateBudgetOverview(budgetId, budgetOverview) {
+  return new Promise((resolve, reject) => {
+    firestore()
+      .collection('budgets')
+      .doc(budgetId)
+      .update(budgetOverview)
+      .then(() => {
+        resolve();
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+export async function getBalanceIncomeExpense(budgetId) {
   return new Promise((resolve, reject) => {
     firestore()
       .collection('budgets')
@@ -21,7 +36,7 @@ export async function getOverview(budgetId) {
   });
 }
 
-export async function getLastFive(budgetId) {
+export async function getLastFiveExpenses(budgetId) {
   return new Promise((resolve, reject) => {
     firestore()
       .collection('budgets')
@@ -31,7 +46,18 @@ export async function getLastFive(budgetId) {
       .limit(5)
       .get()
       .then(querySnapshot => {
-        resolve(querySnapshot.docs);
+        let overviews = [];
+        querySnapshot.docs.forEach(doc => {
+          const expense = doc.data();
+          const overview = {
+            key: doc.id,
+            name: expense.name,
+            amount: expense.amount,
+            date: expense.date,
+          };
+          overviews.push(overview);
+        });
+        resolve(overviews);
       })
       .catch(error => {
         reject(error);
@@ -39,7 +65,7 @@ export async function getLastFive(budgetId) {
   });
 }
 
-export async function addExpense(budgetId, expense) {
+export async function addExpense(budgetId, expense, overview) {
   return new Promise((resolve, reject) => {
     firestore()
       .collection('budgets')
@@ -47,7 +73,96 @@ export async function addExpense(budgetId, expense) {
       .collection('expenses')
       .add(expense)
       .then(docRef => {
-        resolve(docRef.id);
+        overview.balance += expense.amount;
+        if (expense.amount < 0) {
+          overview.expense += expense.amount;
+        } else {
+          overview.income += expense.amount;
+        }
+        updateBudgetOverview(budgetId, overview)
+          .then(() => {
+            console.log('API: expense added with id: ' + docRef.id);
+            console.log('API: new balance: ' + overview.balance);
+            resolve(docRef.id);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+  });
+}
+
+export async function updateExpense(budgetId, expenseId, expense) {
+  return new Promise((resolve, reject) => {
+    firestore()
+      .collection('budgets')
+      .doc(budgetId)
+      .collection('expenses')
+      .doc(expenseId)
+      .update(expense)
+      .then(() => {
+        updateBudgetOverview(budgetId)
+          .then(() => {
+            getBalanceIncomeExpense(budgetId).then(overview => {
+              overview.balance += expense.amount;
+              if (expense.amount < 0) {
+                overview.expense += expense.amount;
+              } else {
+                overview.income += expense.amount;
+              }
+              updateBudgetOverview(budgetId, overview)
+                .then(() => {
+                  console.log('API: expense updated');
+                  console.log('API: new balance: ' + overview.balance);
+                  resolve();
+                })
+                .catch(error => {
+                  reject(error);
+                });
+            });
+          })
+          .catch(error => {
+            reject(error);
+          });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+export async function removeExpense(budgetId, expenseId, amount) {
+  return new Promise((resolve, reject) => {
+    firestore()
+      .collection('budgets')
+      .doc(budgetId)
+      .collection('expenses')
+      .doc(expenseId)
+      .delete()
+      .then(() => {
+        updateBudgetOverview(budgetId)
+          .then(() => {
+            getBalanceIncomeExpense(budgetId).then(overview => {
+              overview.balance += amount;
+              if (amount < 0) {
+                overview.expense += amount;
+              } else {
+                overview.income += amount;
+              }
+              updateBudgetOverview(budgetId, overview)
+                .then(() => {
+                  console.log('API: expense removed');
+                  console.log('API: new balance: ' + overview.balance);
+                  resolve();
+                })
+                .catch(error => {
+                  reject(error);
+                });
+            });
+          })
+          .catch(error => {
+            reject(error);
+          });
       })
       .catch(error => {
         reject(error);
--- app/components/views/budget/BudgetScreen.js
+++ app/components/views/budget/BudgetScreen.js
@@ -7,10 +7,22 @@ import {
   ScrollView,
 } from 'react-native';
 import {useSelector} from 'react-redux';
-import {getOverview, getLastFive} from '../../../api/budgetApi';
-import {Card, ListItem, FAB, Overlay} from 'react-native-elements';
+import {
+  addExpense,
+  getBalanceIncomeExpense,
+  getLastFiveExpenses,
+} from '../../../api/budgetApi';
+import {
+  Card,
+  ListItem,
+  FAB,
+  Overlay,
+  Input,
+  Button,
+} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import NewExpenseScreen from './NewExpenseScreen';
+import {Picker} from '@react-native-picker/picker';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 const BudgetScreen = () => {
   const [history, setHistory] = useState([]);
@@ -18,41 +30,80 @@ const BudgetScreen = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
+  //Overlay add expense state
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState('');
+  const [datePickerShow, setDatePickerShow] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [selectedType, setSelectedType] = useState('');
+
   const budgetId = useSelector(state => state.household.budgetId);
 
   useEffect(() => {
-    getOverview(budgetId)
-      .then(res => {
-        setResume(res);
+    getBudgetOverview(budgetId);
+    getExpensesOverview(budgetId);
+  }, [budgetId]);
+
+  const getBudgetOverview = id => {
+    getBalanceIncomeExpense(id)
+      .then(overview => {
+        setResume(overview);
       })
       .catch(e => {
         console.log(e);
       });
-    getLastFive(budgetId)
-      .then(expenses => {
-        let overviews = [];
-        expenses.forEach(expense => {
-          let overview = {
-            key: expense.id,
-            name: expense.data().name,
-            amount: expense.data().amount,
-            date: expense.data().date,
-          };
-          overviews.push(overview);
-        });
+  };
+
+  const getExpensesOverview = id => {
+    getLastFiveExpenses(id)
+      .then(overviews => {
         setHistory(overviews);
         setIsLoading(false);
       })
       .catch(e => {
         console.log(e);
       });
-  }, [budgetId]);
+  };
 
   const changeOverlayIsVisible = () => {
     if (isOverlayVisible) {
       setIsOverlayVisible(false);
     } else {
       setIsOverlayVisible(true);
+    }
+  };
+
+  const addNewExpense = () => {
+    const expense = {
+      name: name,
+      amount: Number(amount),
+    };
+    addExpense(budgetId, expense, resume)
+      .then(docId => {
+        console.log('SCREEN: expense added with id: ' + docId);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const cancelNewExpense = () => {
+    setDate(new Date());
+    setAmount('');
+    setName('');
+    setSelectedType('');
+    changeOverlayIsVisible();
+  };
+
+  const setPlaceholder = (placeholder, value) => {
+    if (!value) {
+      return placeholder;
+    }
+  };
+
+  const setLabel = (label, value) => {
+    if (value) {
+      return label;
     }
   };
 
@@ -102,9 +153,9 @@ const BudgetScreen = () => {
         </Card>
       </ScrollView>
       <FAB
-        color="grey"
+        color="#FCA311"
         placement="right"
-        icon={<Icon name="add" />}
+        icon={<Icon name="add" color="white" />}
         onPress={changeOverlayIsVisible}
       />
       <Overlay
@@ -117,7 +168,88 @@ const BudgetScreen = () => {
           backgroundColor: 'grey',
           opacity: 0.7,
         }}>
-        <NewExpenseScreen budgetId={budgetId} />
+        <View>
+          <Picker
+            selectedValue={selectedType}
+            onValueChange={value => {
+              setSelectedType(value);
+            }}>
+            <Picker.Item label="dépense" value="expense" />
+            <Picker.Item label="rentrée" value="income" />
+          </Picker>
+          <Input
+            label={setLabel('nom', name)}
+            placeholder={setPlaceholder('nom', name)}
+            value={name}
+            onChangeText={value => {
+              setName(value);
+            }}
+            containerStyle={{}}
+          />
+          <Input
+            label={setLabel('montant', amount)}
+            placeholder={setPlaceholder('montant', amount)}
+            value={amount}
+            keyboardType="numeric"
+            onChangeText={value => {
+              setAmount(value);
+            }}
+          />
+          <Input
+            label="date"
+            value={date.toLocaleDateString()}
+            onFocus={() => {
+              setDatePickerShow(true);
+            }}
+            onBlur={() => {
+              setDatePickerShow(false);
+            }}
+          />
+          {datePickerShow && (
+            <RNDateTimePicker
+              value={new Date()}
+              mode={'date'}
+              display="spinner"
+              onChange={(event, selectedDate) => {
+                const currentDate = selectedDate || date;
+                setDate(currentDate);
+              }}
+            />
+          )}
+          <Button
+            title="ajouter"
+            type="solid"
+            raised={true}
+            onPress={addNewExpense}
+            containerStyle={{
+              backgroundColor: '#FBFBFB',
+              width: '90%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              marginTop: 20,
+            }}
+            buttonStyle={{
+              backgroundColor: '#FCA311',
+            }}
+          />
+          <Button
+            title="annuler"
+            type="solid"
+            raised={true}
+            onPress={cancelNewExpense}
+            containerStyle={{
+              backgroundColor: '#FBFBFB',
+              width: '90%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              marginTop: 20,
+            }}
+            buttonStyle={{
+              backgroundColor: '#FBFBFB',
+            }}
+            titleStyle={{color: '#FCA311'}}
+          />
+        </View>
       </Overlay>
     </View>
   );
@@ -125,10 +257,12 @@ const BudgetScreen = () => {
 
 const styles = StyleSheet.create({
   main_container: {
+    backgroundColor: '#FBFBFB',
     flex: 1,
   },
   balance: {
     margin: 10,
+    borderWidth: 0,
   },
   income_expense: {
     flexDirection: 'row',
@@ -137,14 +271,17 @@ const styles = StyleSheet.create({
     width: '45.8%',
     margin: 10,
     marginRight: 5,
+    borderWidth: 0,
   },
   expense: {
     width: '45.8%',
     margin: 10,
     marginLeft: 5,
+    borderWidth: 0,
   },
   last_five: {
     margin: 10,
+    borderWidth: 0,
   },
 });
 
